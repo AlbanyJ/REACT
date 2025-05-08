8a9f986e2b70b2c1bff3d5ef0b40b16953c043f6
Albany <albanysufyan@gmail.com>
2025-05-08 12:02:06 +0000

    using different css styles

---
+++ new_two/src/App.jsx
@@ -0,0 +1,17 @@
+//Styling react comoonents wit hcss
+//not using external frameworks
+
+//learnt:
+//1.Externaal
+//2.Inline
+//3.CSS modules
+
+import Button from './Button.jsx'
+
+function App() {
+    return(
+      <Button/>
+    )
+}
+
+export default App
