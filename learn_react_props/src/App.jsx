0f3060f83fed2f23e414339132d9f9a3731be8ad
Albany <albanysufyan@gmail.com>
2025-05-08 12:59:02 +0000

    learn't props

---
+++ learn_react_props/src/App.jsx
@@ -0,0 +1,29 @@
+// props = read-only properties that are shared between componeents
+//         A parent component can send data to a child component.
+//         <Component key=value />
+
+
+//propTypes = a mechanism to ensure that the passed value
+//            is of the correct dtatype.
+//            age: propTypes.number
+
+//defaultProps = default values for props in case they are not 
+//               passed from the parent component
+//               name: "Guest" 
+
+
+import Student from './Student.jsx'
+
+function App() {
+    return(
+      <>
+        <Student name="Nasir" age={28} isStudent={true}/>
+        <Student name="Bob" age={54} isStudent={false}/>
+        <Student name="Lamine" age={17} isStudent={true}/>
+        <Student name="Mbappe" age={28} isStudent={false}/>
+        <Student />
+      </>
+    );
+}
+
+export default App
